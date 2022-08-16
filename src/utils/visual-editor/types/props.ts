877965ba798a37c8120e9c9506c05d3f95f9e9c3
Saturday <zhangkairong123@qq.com>
2022-08-16 13:05:35 +0800

    feat- props init

--- src/utils/visual-editor/types/props.ts
+++ src/utils/visual-editor/types/props.ts
@@ -1,3 +1,52 @@
-export type EditorProps = {
-  
+type EditorTableOption = {
+  options: {
+    label: string; // 列显示文本
+    field: string; // 列绑定的字段
+  }[];
+  showKey: string;
 }
+
+type EditorSelectOptions = {
+  label: string;
+  value: string | number | boolean | object;
+  [prop: string]: any;
+}[]
+
+export enum EditorPropsType {
+  /** 输入框 */
+  input = 'input',
+  /** 数字输入框 */
+  inputNumber = 'InputNumber',
+  /** 颜色选择器 */
+  color = 'color',
+  /** 下拉选择器 */
+  select = 'select',
+  /** 表格 */
+  table = 'table',
+  /** 开关 */
+  switch = 'switch',
+  /** 模型绑定选择器 */
+  modelBind = 'ModelBind',
+  /** 可拖拽项 */
+  crossSortable = 'CrossSortable',
+}
+
+export type EditorProps = {
+  type: EditorPropsType
+  /** 表单项标签名称 */
+  label: string;
+  /** 表单项提示说明 */
+  tips?: string;
+  /** 表单域标签的位置 */
+  labelPosition?: string;
+  /** 表单项默认值 */
+  defaultValue?: any;
+} & {
+  /** 可选项 */
+  options?: EditorSelectOptions;
+} & {
+  max?: number;
+  min?: number;
+} & {
+  table?: EditorTableOption;
+};
